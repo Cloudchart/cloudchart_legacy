63cfcd035c635b9d8df963270bc7dfb7f87f4d88
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2012-10-22 22:51:48 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloudchart::Application.config.secret_token = '6571058086d30988f52ad258777c77c06225dc7b1455e8d31231b2db238f048cbae3837410bcdb5b0a41fa090e69b98d7869804f8f2938aecbb8d53510e1ed5d'
