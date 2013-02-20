78779b024add1af82f0285b8229e06bae23746fe
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2013-02-20 23:10:23 +0200

    Edit all in node mode

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -177,7 +177,7 @@ class Chart
   
   def to_text_with_parent(node)
     text = []
-    self.class.find_persons(node.children).each { |n|
+    node.children.each { |n|
       next if n == node
       text << "\t" * (n.depth - node.depth - 1) + n.title
     }
